27316b606ab4c1407717854dbe45eea3845b937e
21951a67h4 <21951a67h4@iare.ac.in>
2025-12-10 17:12:43 +0530

    feat: add @vercel/analytics package and integrate Analytics component into main application

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { ThemeProvider } from './components/theme-provider'
 import { ToastProvider } from './contexts/ToastContext'
@@ -36,6 +37,7 @@ createRoot(document.getElementById('root')!).render(
             <App />
             <Toaster />
             <SpeedInsights />
+            <Analytics />
           </BrowserRouter>
         </ToastProvider>
       </ThemeProvider>
